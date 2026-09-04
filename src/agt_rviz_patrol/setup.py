bfcec4600012c98b7cd3d32b51d72bb9d238913c
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-04 08:15:39 +0800

    feat: install RViz demo config

--- src/agt_rviz_patrol/setup.py
+++ src/agt_rviz_patrol/setup.py
@@ -8,7 +8,10 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/config', ['config/front_sky_three_views.yaml']),
+        ('share/' + package_name + '/config', [
+            'config/front_sky_three_views.yaml',
+            'config/agt_rviz_demo.rviz',
+        ]),
         ('share/' + package_name + '/launch', ['launch/rviz_patrol.launch.py']),
     ],
     install_requires=['setuptools', 'PyYAML'],
