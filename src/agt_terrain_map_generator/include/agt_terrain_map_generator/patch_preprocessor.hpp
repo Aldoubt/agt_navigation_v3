7a9ad1678a6d623184e7467b88450c09565056b3
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-06 20:50:10 +0800

    terrain: add patch preprocessing boundary

---
+++ src/agt_terrain_map_generator/include/agt_terrain_map_generator/patch_preprocessor.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+
+#include "agt_terrain_map_generator/mapping_assets.hpp"
+
+namespace agt_terrain_map_generator
+{
+
+struct PreparedPatch
+{
+  PointCloud local_cloud;
+  RigidTransform3d map_from_local;
+};
+
+class PatchPreprocessor
+{
+public:
+  virtual ~PatchPreprocessor() = default;
+
+  virtual std::string name() const = 0;
+
+  virtual bool process(
+    const PatchAsset & asset,
+    const PointCloud & body_cloud,
+    PreparedPatch & prepared,
+    std::string & error) const = 0;
+};
+
+}  // namespace agt_terrain_map_generator
