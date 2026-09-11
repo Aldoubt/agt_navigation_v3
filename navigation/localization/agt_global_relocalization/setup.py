391da7937242752d19d18c7755063294c223af98
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-12 00:33:42 +0800

    build(localization): install manual seed relocalizer

--- navigation/localization/agt_global_relocalization/setup.py
+++ navigation/localization/agt_global_relocalization/setup.py
@@ -19,5 +19,6 @@ setup(
     license='Apache-2.0',
     entry_points={'console_scripts': [
         'global_relocalization = agt_global_relocalization.global_relocalization:main',
+        'manual_seed_relocalization = agt_global_relocalization.manual_seed_relocalization:main',
     ]},
 )
