0d67c6cb8d097184927ab27e92c9a7faff2b0a33
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-12 12:03:11 +0800

    fix(localization): support sim time in map tracker launch

--- navigation/localization/agt_map_tracker/launch/map_tracker.launch.py
+++ navigation/localization/agt_map_tracker/launch/map_tracker.launch.py
@@ -11,11 +11,13 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('global_map', default_value=''),
         DeclareLaunchArgument('scan_topic', default_value='/agt/livox/points'),
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
         Node(
             package='agt_map_tracker', executable='map_tracker',
             name='agt_map_tracker', output='screen',
             parameters=[str(share / 'config' / 'map_tracker.yaml'), {
                 'global_map': LaunchConfiguration('global_map'),
                 'scan_topic': LaunchConfiguration('scan_topic'),
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
             }]),
     ])
