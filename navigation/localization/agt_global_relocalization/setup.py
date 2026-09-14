54a393e3f07124428c71064fbd5e65cbea33f3dd
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-15 00:16:56 +0800

    test: register pytest suites with colcon

--- navigation/localization/agt_global_relocalization/setup.py
+++ navigation/localization/agt_global_relocalization/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/' + package_name + '/launch', ['launch/global_relocalization.launch.py']),
     ],
     install_requires=['setuptools', 'PyYAML'],
+    tests_require=['pytest'],
     zip_safe=True,
     maintainer='AGT',
     maintainer_email='contact@aldoubt.com',
